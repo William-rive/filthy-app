a6c356c9d6ef96ff64ce1e574fda858ef42bb8f1
William-rive <104038312+William-rive@users.noreply.github.com>
2024-12-06 15:11:15 +0100

    Refactor la structure de la page d'accueil et supprime les polices locales inutilisées

---
+++ app/home/page.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const HomePage: React.FC = () => {
+    return (
+        <div style={{ textAlign: 'center', marginTop: '50px' }}>
+            <h1>Welcome to Filthy App</h1>
+        </div>
+    );
+};
+
+
+
+export default HomePage;

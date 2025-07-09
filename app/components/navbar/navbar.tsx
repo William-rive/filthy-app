190bdf02df04b658ef699b9c131d26038b77c111
William-rive <104038312+William-rive@users.noreply.github.com>
2025-07-09 15:14:16 +0200

    Fix auth session et mise à jour de la logique du composant NavBar

--- app/components/navbar/navbar.tsx
+++ app/components/navbar/navbar.tsx
@@ -1,41 +1,32 @@
+import getServerSession from "next-auth";
+import { authOptions } from "@/app/api/auth/[...nextauth]/route";
 import Link from 'next/link';
 import Image from 'next/image';
 import { LoginButton, LogoutButton } from "./AuthButtons";
-import { auth } from '@/auth/authSetup';
 
 export default async function NavBar() {
+  const session = await getServerSession(authOptions);
 
-    const session = await auth();
-    
-    return (
-        <nav className="bg-gray-900 fixed top-0 left-0 right-0 z-50">
-            <div className='flex items-center justify-between px-6 py-4 '>
-                <div className="text-white text-xl font-bold">
-                    <Link href="/">
-                        <Image src='/assets/logo.png' alt="Logo" width={200} height={200} />
-                    </Link>
-                </div>
-                <div className="flex items-center ">
-                    {/* Onglets */}
-                    <div className="flex gap-4">
-                        <Link href="/tunes" className="text-white">
-                            Tunes
-                        </Link>
-                        <Link href="/liveries" className="text-white">
-                            Liveries
-                        </Link>
-                        <Link href="/services" className="text-white">
-                            Recruitement
-                        </Link>
-                        <Link href="/contact" className="text-white">
-                            Contact
-                        </Link>
-                        <div className='text-white'>
-                        {!session?.user ? <LoginButton /> : <LogoutButton />}
-                        </div>
-                    </div>
-                </div>
+  return (
+    <nav className="bg-gray-900 fixed top-0 left-0 right-0 z-50">
+      <div className='flex items-center justify-between px-6 py-4 '>
+        <div className="text-white text-xl font-bold">
+          <Link href="/">
+            <Image src='/assets/logo.png' alt="Logo" width={200} height={200} />
+          </Link>
+        </div>
+        <div className="flex items-center ">
+          <div className="flex gap-4">
+            <Link href="/tunes" className="text-white">Tunes</Link>
+            <Link href="/liveries" className="text-white">Liveries</Link>
+            <Link href="/services" className="text-white">Recruitement</Link>
+            <Link href="/contact" className="text-white">Contact</Link>
+            <div className='text-white'>
+              {!session ? <LoginButton /> : <LogoutButton />}
             </div>
-        </nav>
-    );
-};
+          </div>
+        </div>
+      </div>
+    </nav>
+  );
+}
